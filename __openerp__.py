0768ead1d4dfda4001543b540b354c0ff20fe2b4
ddizoya <ddizoya@danielcastelao.org>
2016-04-06 10:40:29 +0200

    Base para empezar a hacer el tema.

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+  'name':'Tema Tutorial de David Diz',
+  'description': 'Pruebas para Odoo',
+  'version':'1.0',
+  'author':'David Diz',
+
+ 'data': [
+  'views/layout.xml',
+  'views/pages.xml'
+],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
